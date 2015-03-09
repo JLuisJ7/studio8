bd0b76707ca0c8e7ab9d3fa1ff76268284e097e8
jluisj7 <jluisjab7@gmail.com>
2015-03-08 22:12:39 -0500

    index ordenado

--- wp-content/themes/studio8/index.php
+++ wp-content/themes/studio8/index.php
@@ -1,94 +1,122 @@
 <?php get_header(); ?>
-
-
-
-
   <div class="loader">
-
     <img src="<?php bloginfo('template_url'); ?>/images/loading.svg" />
-  </div>
-  
-    <a href="#" id="arrow_right"><i class="fa fa-long-arrow-right"></i></a>
+  </div>  
+  <a href="#" id="arrow_right"><i class="fa fa-long-arrow-right"></i></a>
   <a href="#" id="arrow_left"><i class="fa fa-long-arrow-left"></i></a>
-    <div id="maximage">
-
-          <div>
-        <img src="<?php bloginfo('template_url'); ?>/images/img-video.png">
-                  <video id="video" autoplay loop poster="<?php bloginfo('template_url'); ?>/images/img-video.png">
-                                      <source src="<?php bloginfo('template_url'); ?>/video/video.mp4" type="video/mp4">
-                      </video>
-                <div class="in-slide-content fade-enable">
-          <section class="intro">
-                          <h1>Motion</h1>
-                                      <h2>Business Lifestyle</h2>
-<a href="#" class="readmore">Case Study</a>                      </section>
-        </div>
-        <div class="gradient"></div>
-                <div id="skip">
-          <a href="#inner-content"><i class="fa fa-3x fa-angle-down pulsing"></i></a>
-        </div>
-              </div>
-            <div>
-        <img src="<?php bloginfo('template_url'); ?>/images/hamburger.jpg">
-                <div class="in-slide-content fade-enable">
-          <section class="intro">
-                          <h1>Branding Identity</h1>
-                                      <h2>Le Roi du Burger</h2>
-<a href="#" class="readmore">Case Study</a>                      </section>
-        </div>
-        <div class="gradient"></div>
-                <div id="skip">
-          <a href="#inner-content"><i class="fa fa-3x fa-angle-down pulsing"></i></a>
-        </div>
-              </div>
-            <div>
-        <img src="<?php bloginfo('template_url'); ?>/images/night.jpeg">
-                <div class="in-slide-content fade-enable">
-          <section class="intro">
-                          <h1>Print</h1>
-                                      <h2>New York</h2>
-<a href="#" class="readmore">Case Study</a>                      </section>
-        </div>
-        <div class="gradient"></div>
-                <div id="skip">
-          <a href="#inner-content"><i class="fa fa-3x fa-angle-down pulsing"></i></a>
-        </div>
-              </div>
-            <div>
-        <img src="<?php bloginfo('template_url'); ?>/images/featured.jpg">
-                <div class="in-slide-content fade-enable">
-          <section class="intro">
-                          <h1>Interactive Design</h1>
-                                      <h2>Howard Taylor</h2>
-<a href="#" class="readmore">Case Study</a>                      </section>
-        </div>
-        <div class="gradient"></div>
-                <div id="skip">
-          <a href="#inner-content"><i class="fa fa-3x fa-angle-down pulsing"></i></a>
-        </div>
-              </div>
-      
+  <div id="maximage">
+    <div>
+      <img src="<?php bloginfo('template_url'); ?>/images/img-video.png">
+      <video id="video" autoplay loop poster="<?php bloginfo('template_url'); ?>/images/img-video.png">
+        <source src="<?php bloginfo('template_url'); ?>/video/video.mp4" type="video/mp4">
+      </video>
+      <div class="in-slide-content fade-enable">
+        <section class="intro">
+          <h1>Motion</h1>
+          <h2>Business Lifestyle</h2>
+          <a href="#" class="readmore">Case Study</a>
+        </section>
+      </div>
+      <div class="gradient"></div>
+      <div id="skip">
+        <a href="#inner-content"><i class="fa fa-3x fa-angle-down pulsing"></i></a>
+      </div>
+    </div>
+    <div>
+      <img src="<?php bloginfo('template_url'); ?>/images/hamburger.jpg">
+      <div class="in-slide-content fade-enable">
+        <section class="intro">
+          <h1>Branding Identity</h1>
+          <h2>Le Roi du Burger</h2>
+          <a href="#" class="readmore">Case Study</a>
+        </section>
+      </div>
+      <div class="gradient"></div>
+      <div id="skip">
+        <a href="#inner-content"><i class="fa fa-3x fa-angle-down pulsing"></i></a>
+      </div>
+    </div>
+    <div>
+      <img src="<?php bloginfo('template_url'); ?>/images/night.jpeg">
+      <div class="in-slide-content fade-enable">
+        <section class="intro">
+          <h1>Print</h1>
+          <h2>New York</h2>
+          <a href="#" class="readmore">Case Study</a>
+        </section>
+      </div>
+      <div class="gradient"></div>
+      <div id="skip">
+        <a href="#inner-content"><i class="fa fa-3x fa-angle-down pulsing"></i></a>
+      </div>
+    </div>
+    <div>
+      <img src="<?php bloginfo('template_url'); ?>/images/featured.jpg">
+      <div class="in-slide-content fade-enable">
+        <section class="intro">
+          <h1>Interactive Design</h1>
+          <h2>Howard Taylor</h2>
+          <a href="#" class="readmore">Case Study</a>
+        </section>
+      </div>
+      <div class="gradient"></div>
+      <div id="skip">
+        <a href="#inner-content"><i class="fa fa-3x fa-angle-down pulsing"></i></a>
+      </div>
+    </div>      
   </div>
-
-  <div class="main" id="inner-content">
-
-    
-    <div class="row cf" style="background-color: transparent;background-size: auto;padding: 72px 0;"><div class="wrap"> 
-<div class="shortcode_text_block" style="background: transparent;width:70%;"><h3 class="title" style="">Finest brands &amp; websites</h3><p class="lead" style="color:;">A super flexible premium WordPress theme for creative professionals, agencies, content creators &amp; businesses.</p></div>
- </div></div>
-<div class="row cf" style="background-size: auto;"> 
-<div class='block-grid projects-listing columns-2 cf'>
-<article class='block-item half-width half-height overlay-expand cf cat_9' ><a href='#' rel='bookmark' target='_self' title='New York'><div class='over'></div><h5>New York<i>Creative Direction</i></h5><div class='image' style='background-image: url();'></div></a></article>
-<article class='block-item half-width half-height overlay-expand cf cat_9 cat_11' ><a href='#' rel='bookmark' target='_self' title='Oldport Restaurant'><div class='over'></div><h5>Oldport Restaurant<i>Creative Direction, Interactive Design</i></h5><div class='image' style='background-image: url();'></div></a></article>
-<article class='block-item half-width half-height overlay-expand cf cat_8 cat_13' ><a href='#' rel='bookmark' target='_self' title='Suavve Magazine'><div class='over'></div><h5>Suavve Magazine<i>Branding Identity, Typography</i></h5><div class='image' style='background-image: url();'></div></a></article>
-<article class='block-item half-width half-height overlay-expand cf cat_8 cat_11' ><a href='#' rel='bookmark' target='_self' title='Le Roi du Burger'><div class='over'></div><h5>Le Roi du Burger<i>Branding Identity, Interactive Design</i></h5><div class='image' style='background-image: url();'></div></a></article>
-</div>
- </div>
-<div class="row cf" style="background-color: transparent;background-size: auto;text-align: center;padding: 48px 0;"><div class="wrap"> 
-<div class="shortcode_text_block" style="background: transparent;width:70%;"><h3 class="title" style="">Let's work together</h3><p class="lead" style="color:;">Ocean burst by, extra feedback goodness, quality plush squeezable terrific easily.<div class="clear cf" style="margin-top:24px;margin-bottom:0"></div><a href="<?php echo get_option('home'); ?>/contacto" target="_self" class="btn medium center black" style="">Contactenos<i class="fa fa-fx fa-envelope" style=""></i></a></p></div>
- </div></div>
-
-    
+  <div class="main" id="inner-content">    
+    <div class="row cf" style="background-color: transparent;background-size: auto;padding: 72px 0;">
+      <div class="wrap"> 
+        <div class="shortcode_text_block" style="background: transparent;width:70%;">
+          <h3 class="title" style="">Finest brands &amp; websites</h3>
+          <p class="lead" style="color:;">A super flexible premium WordPress theme for creative professionals, agencies, content creators &amp; businesses.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row cf" style="background-size: auto;">
+      <div class='block-grid projects-listing columns-2 cf'>
+        <article class='block-item half-width half-height overlay-expand cf cat_9' >
+          <a href='#' rel='bookmark' target='_self' title='New York'>
+            <div class='over'></div>
+            <h5>New York<i>Creative Direction</i></h5>
+            <div class='image' style='background-image: url();'></div>
+          </a>
+        </article>
+        <article class='block-item half-width half-height overlay-expand cf cat_9' >
+          <a href='#' rel='bookmark' target='_self' title='New York'>
+            <div class='over'></div>
+            <h5>New York<i>Creative Direction</i></h5>
+            <div class='image' style='background-image: url();'></div>
+          </a>
+        </article>
+        <article class='block-item half-width half-height overlay-expand cf cat_9' >
+          <a href='#' rel='bookmark' target='_self' title='New York'>
+            <div class='over'></div>
+            <h5>New York<i>Creative Direction</i></h5>
+            <div class='image' style='background-image: url();'></div>
+          </a>
+        </article>
+        <article class='block-item half-width half-height overlay-expand cf cat_9' >
+          <a href='#' rel='bookmark' target='_self' title='New York'>
+            <div class='over'></div>
+            <h5>New York<i>Creative Direction</i></h5>
+            <div class='image' style='background-image: url();'></div>
+          </a>
+        </article>
+      </div>
+    </div>
+    <div class="row cf" style="background-color: transparent;background-size: auto;text-align: center;padding: 48px 0;">
+      <div class="wrap"> 
+        <div class="shortcode_text_block" style="background: transparent;width:70%;">
+          <h3 class="title" style="">Let's work together</h3>
+          <p class="lead" style="color:;">Ocean burst by, extra feedback goodness, quality plush squeezable terrific easily.
+            <div class="clear cf" style="margin-top:24px;margin-bottom:0"></div>
+            <a href="<?php echo get_option('home'); ?>/contacto" target="_self" class="btn medium center black" style="">Contactenos<i class="fa fa-fx fa-envelope" style=""></i></a>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 
 <?php get_footer(); ?>
--- wp-content/themes/studio8/single.php
+++ wp-content/themes/studio8/single.php
@@ -1,51 +1,48 @@
 <?php get_header(); ?>
 <div class="posts inner-content">
-	<div class="entry-header">
-		
-		<img width="1920" height="1280" src="<?php bloginfo('template_url'); ?>/images/architecture-bridge-california-3367.jpg" class="attachment-full wp-post-image" alt="architecture-bridge-california-3367" no-lazy="" />
-</div>
-
-<div class="main wrap">
-<?php if (have_posts()): ?>
-	<?php while(have_posts()): the_post(); ?>
-			<article id="post-551" class="post-551 post type-post status-publish format-standard has-post-thumbnail hentry category-design-principles tag-inspiration">
-		<div class="post-header">
-			<div>
-				<a href="#" rel="category tag">category</a> <span class="sep">/</span> <time><?php the_time('d \d\e F, Y') ?></time> <span class="sep">/</span>
-				<ul class="social">
-					<li>
-						<a id="twbutton" onClick="twwindows('http://twitter.com/intent/tweet?text=<?php the_title();?>&url=<?php the_permalink(); ?>'); return false;"><span class="fa fa-twitter"></span></a>
-					</li>
-					<li>
-						<a id="gpbutton" onclick="gpwindows('https://plus.google.com/share?url=<?php the_permalink();?>'); return false;"><span class="fa fa-google-plus"></span></a>
-					</li>
-					<li>
-						<a id="fbbutton" onclick="fbwindows('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink();?>'); return false;"><span class="fa fa-facebook"></span></a>
-					</li>
-				</ul>
-			</div>
-			<h1 class="post-title"><?php the_title(); ?></h1>
-		</div>
-
-		<div class="post-content">
-			<?php the_content(); ?>
-		</div>
-		<div class="author_description">
-			<div class="author_description_inner">
-				<div class="image">
-					<?php echo get_avatar( get_the_author_meta('email'), '80' ); ?>  
-				</div>
-				<div class="author_text_holder">
-					<h4><?php the_author(); ?></h4>
-				</div>
-			</div>
-		</div>
-	
-	</article>
-	<?php endwhile; ?>		
-<?php endif ?>
-	
-
-</div>
+	<div class="entry-header">		
+		<img width="1920" height="1280" src="<?php bloginfo('template_url'); ?>/images/architecture-bridge-california-3367.jpg" class="attachment-full wp-post-image" alt="" no-lazy="" />
 	</div>
+
+	<div class="main wrap">
+	<?php if (have_posts()): ?>
+		<?php while(have_posts()): the_post(); ?>
+		<article id="post-551" class="post-551 post type-post status-publish format-standard has-post-thumbnail hentry category-design-principles tag-inspiration">
+			<div class="post-header">
+				<div>
+					<a href="#" rel="category tag">category</a> <span class="sep">/</span> <time><?php the_time('d \d\e F, Y') ?></time> <span class="sep">/</span>
+					<ul class="social">
+						<li>
+							<a id="twbutton" onClick="twwindows('http://twitter.com/intent/tweet?text=<?php the_title();?>&url=<?php the_permalink(); ?>'); return false;"><span class="fa fa-twitter"></span></a>
+						</li>
+						<li>
+							<a id="gpbutton" onclick="gpwindows('https://plus.google.com/share?url=<?php the_permalink();?>'); return false;"><span class="fa fa-google-plus"></span></a>
+						</li>
+						<li>
+							<a id="fbbutton" onclick="fbwindows('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink();?>'); return false;"><span class="fa fa-facebook"></span></a>
+						</li>
+					</ul>
+				</div>
+				<h1 class="post-title"><?php the_title(); ?></h1>
+			</div>
+
+			<div class="post-content">
+				<?php the_content(); ?>
+			</div>
+			<div class="author_description">
+				<div class="author_description_inner">
+					<div class="image">
+						<?php echo get_avatar( get_the_author_meta('email'), '80' ); ?>  
+					</div>
+					<div class="author_text_holder">
+						<h4><?php the_author(); ?></h4>
+					</div>
+				</div>
+			</div>
+		
+		</article>
+		<?php endwhile; ?>		
+	<?php endif ?>
+	</div>
+</div>
 <?php get_footer(); ?>
