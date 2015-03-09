9a5af0629ecdab7a0fa02d78d3e1d963da05219a
jluisj7 <jluisjab7@gmail.com>
2015-03-09 18:14:58 -0500

    page empresa.php al 50%

--- wp-content/themes/studio8/page-empresa.php
+++ wp-content/themes/studio8/page-empresa.php
@@ -22,66 +22,89 @@
 					</div>
 				</div>
 			</div>
-<div class="row cf" style="background-size: auto;"><div class="wrap"> 
-<div class="one_third " style="">
-<h6>Web Design</h6>
-<p>Topiary de-jour gold high-rise architectural butler elegant presidential, society ladies pleasure tailored benefactor wishlist ballroom. Guests reserved grande opulent salon property inspiring topiary philanthropic gifted grande educated purebred affluent. Respectable blissfull cigar investments ballroom de-jour portfolio ambassador.</p>
-<a href="#" class="link black" target="_self">Learn more</a>
-</div>
-<div class="one_third " style="">
-<h6>Mobile/App Development</h6>
-<p>Genuine pleasure cigar, dynasty becoming respectable designer, noble status rare inspiring. Purebred cruise noble pedigree, yacht university suite. First-class, elegant panoramic symphony treasure with brilliant imported expensive inspiring five-star de-jour silver. Pearl cocktail topiary, grande wealth cutlery townhome world metropolitan. </p>
-<a href="#" class="link black" target="_self">Learn more</a>
-</div>
-<div class="one_third last " style=""> 
-<h6>Print &amp; Collateral</h6>
-<p>Impressive panoramic opulent polo pedigree theatre dignified imported gifted university ornamental. Cultered metropolitan wishlist, elegant caviar crafted. Silk status architectural wishlist, presidential manor opulent politically lifestyle, investments, yacht. Enthusiast career educated de-jour echelon in pedigree, rare upper opulent portfolio diamond cuisine investments. </p>
-<a href="#" class="link black" target="_self">Learn more</a>
- </div><div class="cf"></div>
- </div></div>
-<div class="clear cf" style="margin-top:72px;margin-bottom:0"></div>
-<div class="row cf" style="background-size: auto;"><div class="wrap"> 
-<h6>Our Team</h6>
-<div class="one_third " style="">
-<div class='shortcode_team'><div class='team_inner'><div class='team_image'><img src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team3.jpg' alt='Ronnie Marshall' class='lazy lazy-hidden square' /><noscript><img src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team3.jpg' alt='Ronnie Marshall' class='square' /></noscript></div><div class='team_text'><div class='team_text_inner'><div class='team_title_holder'><h5 class='team_name'>Ronnie Marshall</h5><span>CEO &amp; Founder</span></div></div></div></div></div>
-</div>
-<div class="one_third " style="">
-<div class='shortcode_team'><div class='team_inner'><div class='team_image'><img src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team2.jpg' alt='Lauren Voss' class='lazy lazy-hidden square' /><noscript><img src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team2.jpg' alt='Lauren Voss' class='square' /></noscript></div><div class='team_text'><div class='team_text_inner'><div class='team_title_holder'><h5 class='team_name'>Lauren Voss</h5><span>Lead Designer</span></div></div></div></div></div>
-</div>
-<div class="one_third last " style=""> 
-<div class='shortcode_team'><div class='team_inner'><div class='team_image'><img src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team4.jpg' alt='Steven Davis' class='lazy lazy-hidden square' /><noscript><img src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team4.jpg' alt='Steven Davis' class='square' /></noscript></div><div class='team_text'><div class='team_text_inner'><div class='team_title_holder'><h5 class='team_name'>Steven Davis</h5><span>Web Developer</span></div></div></div></div></div>
- </div><div class="cf"></div>
-<div class="clear cf" style="margin-top:48px;margin-bottom:0"></div>
-<div class="one_third " style="">
-<div class='shortcode_team'><div class='team_inner'><div class='team_image'><img src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team1.jpg' alt='Laura James' class='lazy lazy-hidden square' /><noscript><img src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team1.jpg' alt='Laura James' class='square' /></noscript></div><div class='team_text'><div class='team_text_inner'><div class='team_title_holder'><h5 class='team_name'>Laura James</h5><span>Chief Happiness Officer</span></div></div></div></div></div>
-</div>
-<div class="one_third " style="">
-<div class='shortcode_team'><div class='team_inner'><div class='team_image'><img src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team5.jpg' alt='Jonathan Pearson' class='lazy lazy-hidden square' /><noscript><img src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team5.jpg' alt='Jonathan Pearson' class='square' /></noscript></div><div class='team_text'><div class='team_text_inner'><div class='team_title_holder'><h5 class='team_name'>Jonathan Pearson</h5><span>Designer</span></div></div></div></div></div>
-</div>
-<div class="one_third last " style=""> 
-<div class='shortcode_team'><div class='team_inner'><div class='team_image'><img src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team6.jpg' alt='Ryan Brooks' class='lazy lazy-hidden square' /><noscript><img src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team6.jpg' alt='Ryan Brooks' class='square' /></noscript></div><div class='team_text'><div class='team_text_inner'><div class='team_title_holder'><h5 class='team_name'>Ryan Brooks</h5><span>Web Developer</span></div></div></div></div></div>
- </div><div class="cf"></div>
- </div></div>
-<div class="clear cf" style="margin-top:72px;margin-bottom:0"></div>
-<div class="row cf" style="background-size: auto;padding: 0;"><div class="wrap"> 
-<div class="carousel-images no-lazy"><h6 class="title" style="color: #000;">Our Brands</h6><div class="carousel-owl-images owl-carousel">
-<div><img class="lazy lazy-hidden" alt="Mashable" src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/mashable.svg"><noscript><img alt="Mashable" src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/mashable.svg"></noscript></div>
-<div><img class="lazy lazy-hidden" alt="Calvein Klein" src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/calvinklein.svg"><noscript><img alt="Calvein Klein" src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/calvinklein.svg"></noscript></div>
-<div><img class="lazy lazy-hidden" alt="Times" src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/losangelestimes.svg"><noscript><img alt="Times" src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/losangelestimes.svg"></noscript></div>
-<div><img class="lazy lazy-hidden" alt="Vanity Fair" src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/vanityfair.svg"><noscript><img alt="Vanity Fair" src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/vanityfair.svg"></noscript></div>
-<div><img class="lazy lazy-hidden" alt="Hearts" src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/hearts.svg"><noscript><img alt="Hearts" src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/hearts.svg"></noscript></div>
-<div><img class="lazy lazy-hidden" alt="Essie" src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/essie.svg"><noscript><img alt="Essie" src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/essie.svg"></noscript></div>
-<div><img class="lazy lazy-hidden" alt="In Style" src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/instyle.svg"><noscript><img alt="In Style" src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/instyle.svg"></noscript></div>
-<div><img class="lazy lazy-hidden" alt="Finlandia" src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/finlandia.svg"><noscript><img alt="Finlandia" src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/finlandia.svg"></noscript></div>
-<div><img class="lazy lazy-hidden" alt="Interview" src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/interview.svg"><noscript><img alt="Interview" src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/interview.svg"></noscript></div>
-</div></div>
- </div></div>
-<div class="clear cf" style="margin-top:48px;margin-bottom:0"></div>
-
-
-				
+			<div class="row cf" style="background-size: auto;">
+				<div class="wrap">
+					<?php if (have_posts()): ?>
+							<?php while(have_posts()): the_post();?>								
+								<?php the_content(); ?>
+							<?php endwhile; ?>
+							<?php endif ?>	
+				</div>
 			</div>
+			<div class="clear cf" style="margin-top:72px;margin-bottom:0"></div>
+			<div class="row cf" style="background-size: auto;">
+				<div class="wrap">
+					<h6>Nuestro Equipo</h6>
+					<div class="one_third " style="">
+						<div class='shortcode_team'>
+							<div class='team_inner'>
+								<div class='team_image'>
+									<img src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team3.jpg' alt='Ronnie Marshall' class='lazy lazy-hidden square' />
+									<noscript>
+										<img src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team3.jpg' alt='Ronnie Marshall' class='square' />
+									</noscript>
+								</div>
+								<div class='team_text'>
+									<div class='team_text_inner'>
+										<div class='team_title_holder'>
+											<h5 class='team_name'>Ronnie Marshall</h5>
+											<span>CEO &amp; Founder</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="one_third " style="">
+						<div class='shortcode_team'>
+							<div class='team_inner'>
+								<div class='team_image'>
+									<img src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team3.jpg' alt='Ronnie Marshall' class='lazy lazy-hidden square' />
+									<noscript>
+										<img src='https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Assets/team3.jpg' alt='Ronnie Marshall' class='square' />
+									</noscript>
+								</div>
+								<div class='team_text'>
+									<div class='team_text_inner'>
+										<div class='team_title_holder'>
+											<h5 class='team_name'>Ronnie Marshall</h5>
+											<span>CEO &amp; Founder</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>					
+				</div>
+			</div>
+			<div class="clear cf" style="margin-top:72px;margin-bottom:0"></div>
+			<div class="row cf" style="background-size: auto;padding: 0;">
+				<div class="wrap">
+					<div class="carousel-images no-lazy">
+						<?php
+							$args = array(
+								'post_type' => 'page',
+								'posts_per_page' => 1,
+								'ID' => 82
+								);
+								$query = new WP_Query( $args );
+								if ( $query->have_posts() ) : ?>
+									<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+										<h6 class="title" style="color: #000;"><?php the_title(); ?></h6>
+									<div class="carousel-owl-images owl-carousel">
 
+										<div>
+
+											<img class="lazy lazy-hidden" alt="Mashable" src="data:image/gif;base64,R0lGODdhAQABAPAAAP///wAAACwAAAAAAQABAEACAkQBADs=" data-lazy-type="image" data-lazy-src="https://dl.dropboxusercontent.com/u/1699200/Envato/Themes/Studio%208/Images/Logos/mashable.svg">
+										</div>							
+									</div>
+									<?php endwhile; ?>
+								<?php endif ?>
+					</div>
+				</div>
+			</div>
+			<div class="clear cf" style="margin-top:48px;margin-bottom:0"></div>
 		</div>
+	</div>
 
 <?php get_footer(); ?>
