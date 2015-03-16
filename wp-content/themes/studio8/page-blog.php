28b2b209f0a453cbee5b34e5575626a711c45836
jluisj7 <jluisjab7@gmail.com>
2015-03-16 11:05:04 -0500

    paginación del blog terminado

--- wp-content/themes/studio8/page-blog.php
+++ wp-content/themes/studio8/page-blog.php
@@ -5,34 +5,28 @@
 ?>
 <?php get_header(); ?>
 
-<?php 
-$id = $_GET['id'];
-global $post;  
-?>
 <div class="main inner-content cf">
 	<div class="posts featured cf">
-		<?php
-			$the_query= new WP_Query(
-				array(
-		    			'order' => 'DESC',
-		        		'posts_per_page' => 3,
-		        		'post__in'  => get_option( 'sticky_posts' ),
-		 		       	'ignore_sticky_posts' => 1
-		        	)
-			);
-		?>	
-		 <?php if ($the_query->have_posts()): ?>
-			<?php while($the_query->have_posts()): $the_query->the_post(); ?>
-				<?php		           
-		            $fullimage = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
-            		$urlfullimage = $fullimage['0'];
-				?>
-				
-				<article id="" class="post hentry  full-width full-height">
+		
+<?php if (have_posts()) : ?>
+<?php //if (!is_paged()): ?>
+<?php $sticky = get_option( 'sticky_posts' ); ?>
+<?php if ( isset( $sticky[0] ) ) { 
+		$args = array(
+		'post_type' => 'post',
+		'posts_per_page' => 3,
+		'post__in'  => $sticky,
+		'ignore_sticky_posts' => 1);
+$featured_query = new WP_query( $args );
+while ($featured_query->have_posts() ) :
+$featured_query->the_post();
+$urlImagenDes = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+?>
+<article id="" class="post hentry  full-width full-height">
 					<div class='over'></div>
 					<div class="post-image">
 
-						<img width="1280" height="1280" src="<?php echo $urlfullimage; ?>" class="attachment-thumb-1280 wp-post-image" alt=""  />
+						<img width="1280" height="1280" src="<?php echo $urlImagenDes; ?>" class="attachment-thumb-1280 wp-post-image" alt=""  />
 						<div class="post-header">
 							<div>
 								<a href="#" rel="category tag">Categoria</a> <span class="sep">/</span> <time><?php the_time('d \d\e F, Y') ?></time>
@@ -42,37 +36,31 @@ global $post;
 							</h3>
 						</div>
 					</div>
-				</article>
-			<?php endwhile; ?>		
-		<?php endif ?>
-		<?php wp_reset_postdata(); ?>		
-	</div>
-	<div class="wrap">
-		<div class="posts normal cf">
-			<?php 
+</article>
+<?php
+endwhile;
+wp_reset_query();
+}
+?>
+			
+<?php //endif;//paged ?>
+</div>
+<div class="wrap">
+	<div class="posts normal cf">	
+<?php 
+$paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+$sticky = get_option( 'sticky_posts' );
+$args = array(
+'post_type' => 'post',
+'ignore_sticky_posts' => 1,
+'post__not_in' => $sticky,
+'paged' => $paged,
+);
+query_posts( $args );
 
-  $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
-  
-  $custom_args = array(
-      'post_type' => 'post',
-      'posts_per_page' => 4,
-      'paged' => $paged,
-      'ignore_sticky_posts' => 1
-    );
-
-  $custom_query = new WP_Query( $custom_args ); ?>
- 
-  <?php if ( $custom_query->have_posts() ) : ?>
-   <?php 
-   global $post;
-    
-            					?>
-    <!-- the loop -->
-    <?php while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
-    <?php 
+while ( have_posts() ) : the_post();
 $fullimage = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
-            					$urlfullimage = $fullimage['0'];  
-     ?>
+            					$urlfullimage = $fullimage['0'];  ?>
 <article id="post-188" class="post-188 post type-post status-publish format-standard has-post-thumbnail hentry category-news tag-negative-space cf" style="background: url('<?php echo $urlfullimage;  ?>') right no-repeat;background-size:cover;">
 				<div class="post-excerpt m-all t-2of3 d-3of7">
 					<div class="post-header">
@@ -90,26 +78,23 @@ $fullimage = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'ful
 						</div>
 					</div>
 				</div>
-			</article>
+</article>	
+<?php 
+endwhile;
+?>
 
-     <?php endwhile; ?>
+			
+
+     
     <!-- end of the loop -->
 	<nav class="navigation paging-navigation" role="paginacion">		
 		<div class="pagination loop-pagination">
-			 <!-- pagination here -->
-    <?php
-      if (function_exists(custom_pagination)) {
-        custom_pagination($custom_query->max_num_pages,"",$paged);
-      }
-    ?>
- 
-  <?php wp_reset_postdata(); ?>
- 
-  <?php else:  ?>
-    <p><?php _e( 'Sorry, no posts matched.' ); ?></p>
-  <?php endif; ?></div>
+			<?php get_template_part('pagination'); ?>
+		</div>
 	</nav><!--/ .navigation -->								
-						
+<?php else : ?>
+<h3 class="archive-title">Disculpe, no hay art&iacute;culos en la categor&iacute;a <i class="icon-leaf"></i><strong></strong>.</h3>
+<?php endif; ?>						
 				
 
 			
