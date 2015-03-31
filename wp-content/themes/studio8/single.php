76c2fe89b16475313f8532378820500d7b063c3c
jluisj7 <jluisjab7@gmail.com>
2015-03-31 13:25:43 -0500

    single.php sin fondo la imagen destacada

--- wp-content/themes/studio8/single.php
+++ wp-content/themes/studio8/single.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+
 <div class="posts inner-content">
 	<?php if (have_posts()): ?>
 	<?php while(have_posts()): the_post(); ?>
@@ -6,9 +7,22 @@
 		$fullimage = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
         $urlfullimage = $fullimage['0']; 
 	 ?>
-	<div class="entry-header">		
-		<img width="1920" height="1280" src="<?php echo $urlfullimage; ?>" class="attachment-full wp-post-image" alt="" no-lazy="" />
-	</div>
+	<div class="entry-content">
+<div class="row cf" style="background-color: #222;background-size: auto;">
+				<div class="wrap">
+					<div class="shortcode_padding" style="padding-top: 72px;padding-bottom: 72px;padding-left: 0;padding-right: 0;">
+						<div class="full_width " style="">
+																						
+								<h6 class="gray"></h6>
+								<h1 class="page-title white h2" style="color:white;"><?php the_title(); ?></h1>
+															
+							
+						</div>
+						<div class="cf"></div>
+					</div>
+				</div>
+			</div>
+</div>
 
 	<div class="main wrap">
 	
@@ -28,7 +42,9 @@
 						</li>
 					</ul>
 				</div>
-				<h1 class="post-title"><?php the_title(); ?></h1>
+				<div style="text-align:center;">
+					<img src="<?php echo $urlfullimage; ?>" class="attachment-full wp-post-image" alt="" no-lazy="" />
+				</div>
 			</div>
 
 			<div class="post-content">
